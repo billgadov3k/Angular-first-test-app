55874d3cc36733d9e592fe2155235bd9b6c2a174
Aleksandr Tugolukov <billgadov3k@gmail.com>
2022-09-08 23:28:47 +0300

    first commit add app if Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainTablesComponent } from './main-tables/main-tables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainTablesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
